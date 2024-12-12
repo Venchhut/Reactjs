732e766830402c14c49017a2cbf5f49ba93ab968
Venchhut <ijinbaekk@gmail.com>
2024-12-12 14:08:56 +0700

    simple fetching data

--- Data-fetching/src/App.tsx
+++ Data-fetching/src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
+const BaseURL = "https://jsonplaceholder.typicode.com";
+interface Post {
+  id: number;
+  title: string;
+}
+
 const App = () => {
-  return <div>App</div>;
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const respone = await fetch(`${BaseURL}/posts`);
+      const data = await respone.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <h1>Data fetching</h1>
+      <ul>
+        {posts.map((post) => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
