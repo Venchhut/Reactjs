98649288ed6f6f3d0da353d517ab6624231780ac
Venchhut <ijinbaekk@gmail.com>
2025-02-05 09:50:42 +0700

    feat: useState string

--- useState/src/App.tsx
+++ useState/src/App.tsx
@@ -1,63 +1,16 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("");
-  const [allMemeImgs, setAllMemeImgs] = useState<{ url: string }[]>([]);
-  const [randomImg, setRandomImg] = useState("");
-
-  useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((response) => response.json())
-      .then((content) => setAllMemeImgs(content.data.memes));
-  }, []);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    if (name === "topText") setTopText(value);
-    if (name === "bottomText") setBottomText(value);
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (allMemeImgs.length > 0) {
-      const rand =
-        allMemeImgs[Math.floor(Math.random() * allMemeImgs.length)].url;
-      setRandomImg(rand);
-    }
-  };
+  const [inputText, setInputText] = useState("");
 
   return (
     <div>
-      <form className="meme-form" onSubmit={handleSubmit}>
-        <input
-          placeholder="Enter Text"
-          type="text"
-          value={topText}
-          name="topText"
-          onChange={handleChange}
-        />
-        <br />
-        <input
-          placeholder="Enter Text"
-          type="text"
-          value={bottomText}
-          name="bottomText"
-          onChange={handleChange}
-        />
-        <button>Generate</button>
-      </form>
-      <br />
-      <div className="meme">
-        {randomImg && (
-          <>
-            <img src={randomImg} alt="meme" />
-            <h2 className="top">{topText}</h2>
-            <h2 className="bottom">{bottomText}</h2>
-          </>
-        )}
-      </div>
+      <input
+        placeholder="enter username"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <p>useName: {inputText}</p>
     </div>
   );
 };
