8a902d55954141af42129ddc17c74e1eef4131d6
Venchhut <ijinbaekk@gmail.com>
2025-02-05 10:23:26 +0700

    object state

--- useState/src/App.tsx
+++ useState/src/App.tsx
@@ -20,19 +20,48 @@
 // export default App;
 
 // 2 Boolean State
+
+// import { useState } from "react";
+// import "./App.css";
+// const App = () => {
+//   const [isOn, setIsOn] = useState(false);
+//   return (
+//     <div className="container">
+//       <div className="box">
+//         <div className="toggle">
+//           <p>{isOn ? "Enabled" : "Disabled"}</p>
+//           <button onClick={() => setIsOn((prev) => !prev)}>
+//             {isOn ? "ON" : "OFF"}
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// 2 object state
 import { useState } from "react";
-import "./App.css";
 const App = () => {
-  const [isOn, setIsOn] = useState(false);
+  const [username, setUsername] = useState({
+    name: "welcome",
+    age: 20,
+  });
+  const handleClick = () => {
+    setUsername((prev) => ({
+      ...prev,
+      age: prev.age + 1,
+      name: prev.name + "!",
+    }));
+  };
   return (
-    <div className="container">
-      <div className="box">
-        <div className="toggle">
-          <p>{isOn ? "Enabled" : "Disabled"}</p>
-          <button onClick={() => setIsOn((prev) => !prev)}>
-            {isOn ? "ON" : "OFF"}
-          </button>
-        </div>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div>
+        <h2>Student</h2>
+        <p>Name:{username.name}</p>
+        <p>Age:{username.age}</p>
+        <button onClick={handleClick}>Update</button>
       </div>
     </div>
   );
