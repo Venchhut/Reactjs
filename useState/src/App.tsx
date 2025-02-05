d3beb2dddbd87754a55929684fcbcd8c09e0fc10
Venchhut <ijinbaekk@gmail.com>
2025-02-05 10:08:50 +0700

    boolean state

--- useState/src/App.tsx
+++ useState/src/App.tsx
@@ -1,16 +1,39 @@
+// string with state
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [inputText, setInputText] = useState("");
+
+//   return (
+//     <div>
+//       <input
+//         placeholder="enter username"
+//         value={inputText}
+//         onChange={(e) => setInputText(e.target.value)}
+//       />
+//       <p>useName: {inputText}</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// 2 Boolean State
 import { useState } from "react";
-
+import "./App.css";
 const App = () => {
-  const [inputText, setInputText] = useState("");
-
+  const [isOn, setIsOn] = useState(false);
   return (
-    <div>
-      <input
-        placeholder="enter username"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <p>useName: {inputText}</p>
+    <div className="container">
+      <div className="box">
+        <div className="toggle">
+          <p>{isOn ? "Enabled" : "Disabled"}</p>
+          <button onClick={() => setIsOn((prev) => !prev)}>
+            {isOn ? "ON" : "OFF"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
